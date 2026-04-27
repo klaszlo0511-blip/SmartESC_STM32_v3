14d61774312b9b633dc058ece7c0e1181f6e803e
klaszlo0511-blip <k.laszlo0511@gmail.com>
2026-04-27 15:04:35 +0200

    Adjust gear ratio and speed limits in config.h
    Updated gear ratio and speed limits for different modes.

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -44,12 +44,12 @@
 
 // parameters for speed calculation
 #define WHEEL_CIRCUMFERENCE 550 //690 for original M365 motor
-#define GEAR_RATIO 11 //15 for original M365 motor
+#define GEAR_RATIO 20 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 15
+#define SPEEDLIMIT_NORMAL 25
+#define SPEEDLIMIT_SPORT 40
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
 #define PH_CURRENT_MAX_ECO 16000
